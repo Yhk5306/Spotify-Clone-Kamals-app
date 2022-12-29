4d52771085e365934700fefdaba508ac76a5a99d
yusuf hakan kotiloglu <yusufhakankotiloglu@gmail.com>
2022-12-29 10:13:12 +0300

    Revert "wrongly deleted files"
    This reverts commit f2be2eb9

--- back/app.js
+++ back/app.js
@@ -13,6 +13,7 @@ const SpotifyRoute = require('./router/spotifyRouter');
 const SearchRoute = require('./router/searchRouter');
 const IndexRoute = require('./router/IndexRoute');
 const PlayerRouter = require('./router/PlayerRouter');
+const ArtistRouter = require('./router/artistRouter');
 
 const app = express();
 
@@ -50,6 +51,7 @@ app.use(SpotifyRoute);
 app.use(SearchRoute);
 app.use(IndexRoute);
 app.use(PlayerRouter);
+app.use(ArtistRouter)
 
 var generateRandomString = function(length) {
     var text = '';
